daf74435817969e1b81ac73778bdbdb72c3d5532
mmoylam-urgently <99926671+mmoylam-urgently@users.noreply.github.com>
2022-02-24 15:25:09 -0600

    Update RegistrarApplicationTests.java
    Adding outline of full CRUD test

--- src/test/java/com/geturgently/lindquist/registrar/RegistrarApplicationTests.java
+++ src/test/java/com/geturgently/lindquist/registrar/RegistrarApplicationTests.java
@@ -10,4 +10,17 @@ class RegistrarApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void fullCRUDTest(){
+		
+		// Step 1: Connect to Database
+		// Step 2: Create Student through API
+		// Step 3: Retrieve (GET) Student through API
+		// Step 4: Update Student through JDBC
+		// Step 5: Retrieve (GET) Student through API
+		// Step 6: Delete Student through JDBC
+		// Step 7: Try to Retrieve (GET) Student through API
+
+	}
+
 }
